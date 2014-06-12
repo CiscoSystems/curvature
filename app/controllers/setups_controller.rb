c0b46be6782f11b3f1b6db4197fc16eae42dd303
Sam Betts <sam@code-smash.net>
2014-06-12 11:54:49 +0100

    Removed need for predefined empty config files.

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -12,19 +12,15 @@ class SetupsController < ApplicationController
   end
 
   def create
-    if(File.exists?("config/curvature.yml"))
-      @ip = params[:ipaddr]
-      @port = params[:port]
-      config = render_to_string "curvature.yml", :layout => false
-      File.open("config/curvature.yml", "w") { |file|
-        file.write(config)
-      }
-      APP_CONFIG['identity'] = {}
-      APP_CONFIG['identity']['ip'] = @ip
-      APP_CONFIG['identity']['port'] = @port
-      redirect_to login_url, :notice => "Curvature Keystone Config Completed!"
-    else
-      redirect_to setups_url
-    end
+    @ip = params[:ipaddr]
+    @port = params[:port]
+    config = render_to_string "curvature.yml", :layout => false
+    File.open("config/curvature.yml", "w") { |file|
+      file.write(config)
+    }
+    APP_CONFIG['identity'] = {}
+    APP_CONFIG['identity']['ip'] = @ip
+    APP_CONFIG['identity']['port'] = @port
+    redirect_to login_url, :notice => "Curvature Keystone Config Completed!"
   end
 end
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,8 @@
 require 'yaml'
 
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/curvature.yml")[Rails.env]
+begin
+  configfile = YAML.load_file("#{Rails.root}/config/curvature.yml")
+  APP_CONFIG = file[Rails.env]
+rescue
+  APP_CONFIG = { :no_config => true }
+end
