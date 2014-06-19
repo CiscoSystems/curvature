6aa248dd622f95be32955719422a5955add1028d
Bradley Jones <jones.bradley@me.com>
2014-06-19 15:01:05 +0100

    environments form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to edit_user_path(@user), notice: 'User was successfully created.' }
+        #format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password)
+      params.require(:user).permit(:username, :password, :firstname, :surname, :email)
     end
 end
