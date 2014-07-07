8b6e9f6189d4f54cae276386d308d6a2b346471c
Sam Betts <sam@code-smash.net>
2014-07-07 13:41:22 +0100

    Added cookie storage to logins

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,17 +1,32 @@
 class LoginsController < ApplicationController
-  def show
-  end
-
   def destroy
+    @user.environments.each do |env|
+      response = post_request(env.url+"/logout", {})
+      remove_store(env.name, cookies[env.name])
+    end
+
+    cookies[:current_user] = nil
   end
 
   def create
     @user = User.find_by username: params[:username]
+    # Check user login information
     if @user.password = params[:password]  
+      # Set cookie current user id.
+      cookies[:current_user] = @user.id
       # Sign into every environment this user has setup
-      user.environments.each do |env|  
-        response = post_request(env.url, {})
-      end
+      environments_signin
+    end
+  end
+
+  private
+
+  def environments_signin
+    @user.environments.each do |env|  
+      # Fire off sign in request.
+      response = post_request(env.url+"/login", {})
+      # Store cookie from login request response. 
+      store env.name, response.response['set-cookie']
     end
   end
 end
