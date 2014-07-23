329c305ca816bf648218054bfaf57aa7105588dc
Sam Betts <sam@code-smash.net>
2014-07-23 17:22:03 +0100

    Refixed bug because of a full stop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :require_login, only: [:new. :create]
+  skip_before_action :require_login, only: [:new, :create]
 
   # GET /users
   # GET /users.json
